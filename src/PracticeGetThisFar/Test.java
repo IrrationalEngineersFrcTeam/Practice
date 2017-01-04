beb9d95162eb750051ed51f9ebcc608bfa4d267d
Elias Palmer <epalmer004@gmail.com>
2017-01-03 22:53:21 -0500

    no message

---
+++ src/PracticeGetThisFar/Test.java
@@ -0,0 +1,18 @@
+package PracticeGetThisFar;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		
+		System.out.println("If you Got this your done")
+	}
+
+}
+
+
+
+
+
+//h
